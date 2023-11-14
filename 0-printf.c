1a0a3018818f7d91c5686723bdd4680baf464371
hichamelg110 <elisejuneaileen@gmail.com>
2023-11-14 11:22:55 -0800

    printf

--- 0-printf.c
+++ 0-printf.c
@@ -24,3 +24,22 @@ char c = va_arg(countargs, int);
 putchar(c);
 i++;
 }
+else if (*format == 's')
+{
+char *str = va_arg(countargs, char*);
+i += print_string(str);
+}
+else if (*format == '%')
+{
+putchar('%');
+}
+}
+else
+{
+putchar(*format);
+i++;
+}
+}
+va_end(countargs);
+return (i);
+}
