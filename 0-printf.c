58d7542b5661ab5c063750718744270f0eabf769
hichamelg110 <elisejuneaileen@gmail.com>
2023-11-14 05:11:06 -0800

    printf

--- 0-printf.c
+++ 0-printf.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * _printf - a function that produces output according to a format.
+ * @format: format string
+ * Return: the number of characters printed
+ */
 
 int _printf(const char *format, ...)
 {
